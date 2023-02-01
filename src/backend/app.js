8efa3e3f9177fa362ecdd4fa87cb369e8b3246ea
Kositthai <vkositthai@outlook.com>
2023-02-01 10:23:12 +0100

    reconnect db with .evn

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const Knex = require("knex");
 const { first } = require("./database");
+const knex = require("./database");
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -23,17 +24,6 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
-const db = Knex({
-  client: "mysql2",
-  connection: {
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "123456789",
-    database: "meal-sharing",
-  },
-});
-
 // ------- Routes --------- //
 
 app.get("/my-route", (req, res) => {
@@ -41,29 +31,40 @@ app.get("/my-route", (req, res) => {
 });
 
 app.get("/future-meals", async (req, res) => {
-  const [futureMeals] = await db.raw("SELECT meal.title, meal.when FROM meal WHERE meal.when > DATE(NOW());");
-  futureMeals.length != 0 ? res.json({ futureMeals }) : res.json([])
+  const [futureMeals] = await knex.raw(
+    "SELECT meal.title, meal.when FROM meal WHERE meal.when > DATE(NOW());"
+  );
+  futureMeals.length != 0 ? res.json({ futureMeals }) : res.json([]);
 });
 
 app.get("/past-meals", async (req, res) => {
-  const [pastMeals] = await db.raw("SELECT meal.title, meal.when FROM meal WHERE meal.when < DATE(NOW());");
-  pastMeals.length != 0 ? res.json({ pastMeals }) : res.json([])
+  const [pastMeals] = await knex.raw(
+    "SELECT meal.title, meal.when FROM meal WHERE meal.when < DATE(NOW());"
+  );
+  pastMeals.length != 0 ? res.json({ pastMeals }) : res.json([]);
 });
 
 app.get("/all-meals", async (req, res) => {
-  const [allMeals] = await db.raw("SELECT * FROM meal ORDER BY meal.id ASC;");
-  allMeals.length != 0 ? res.json({ allMeals }) : res.json([])
+  const [allMeals] = await knex.raw("SELECT * FROM meal ORDER BY meal.id ASC;");
+  allMeals.length != 0 ? res.json({ allMeals }) : res.json([]);
 });
 
 app.get("/first-meal", async (req, res) => {
-  const [firstMeal] = await db.raw("SELECT * FROM meal ORDER BY meal.id ASC LIMIT 1;");
-  firstMeal.length != 0 ? res.json(firstMeal[0]) : res.status(404).send('There are no meals');
+  const [firstMeal] = await knex.raw(
+    "SELECT * FROM meal ORDER BY meal.id ASC LIMIT 1;"
+  );
+  firstMeal.length != 0
+    ? res.json(firstMeal[0])
+    : res.status(404).send("There are no meals");
 });
 
-
 app.get("/last-meal", async (req, res) => {
-  const [lastMeal] = await db.raw("SELECT * FROM meal ORDER BY meal.id DESC LIMIT 1; ");
-  lastMeal.length != 0 ? res.json(lastMeal[0]) : res.status(404).send('There are no meals');
+  const [lastMeal] = await knex.raw(
+    "SELECT * FROM meal ORDER BY meal.id DESC LIMIT 1; "
+  );
+  lastMeal.length != 0
+    ? res.json(lastMeal[0])
+    : res.status(404).send("There are no meals");
 });
 
 if (process.env.API_PATH) {
