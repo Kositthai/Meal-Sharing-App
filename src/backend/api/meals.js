35369cdbfd510d70f9a2e0231a2935ef26d9bb1c
Kositthai <vkositthai@outlook.com>
2023-02-07 22:28:53 +0100

    api/meals endpoint

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,12 +2,76 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
-router.get("/", async (request, response) => {
+router.use(express.json());
+
+// Returns all meals
+router.get("/", async (req, res) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    const getMeals = await knex("meal").select("*");
+    getMeals.length != 0
+      ? res.json(getReservationList)
+      : res.status(404).send("No meals list");
   } catch (error) {
+    res.status(500).send({ error: error });
+    throw error;
+  }
+});
+
+// Adds a new meal to the database
+router.post("/", async (req, res) => {
+  const mealInfo = req.body;
+  try {
+    const insertMeal = await knex("meal").insert(mealInfo);
+    insertMeal
+      ? res.status(201).send("Successfully inserted meal")
+      : res.status(400).send("Failed to create");
+  } catch (error) {
+    res.status(500).send({ error: error });
+    throw error;
+  }
+});
+
+// Returns the meal by id
+router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const getMealById = await knex("meal").select("*").where({ id });
+    getMealById.length != 0
+      ? res.json(getMealById)
+      : res.status(404).send(`No meals available with this ${id}`);
+  } catch (error) {
+    res.status(500).send({ error: error });
+    throw error;
+  }
+});
+
+// Updates the meal by id
+router.put("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const changeInfo = req.body; //access body in postman body
+  try {
+    const updateMealById = await knex("meal")
+      .where("id", id)
+      .update(changeInfo);
+    updateMealById
+      ? res.status(200).send({ message: "Successfully updated meal" })
+      : res.status(400).send("Failed to update");
+  } catch (error) {
+    res.status(500).send({ error: error });
+    throw error;
+  }
+});
+
+// Deletes the meal by id
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const deleteMealById = await knex("meal").where("id", id).del();
+    deleteMealById
+      ? res.status(200).send({ message: "Successfully delated meal" })
+      : res.status(400).send("Failed to delete");
+  } catch (error) {
+    res.status(500).send({ error: error });
     throw error;
   }
 });
